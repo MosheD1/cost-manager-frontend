e2d0eda892e367c4b3ee905c7f435e7f9680d4ec
moshed1 <moshedego@gmail.com>
2023-09-17 10:44:34 +0300

    add comments to the app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+/*
+    Moshe Dego 315044511
+    Omri Elbaz 315006635
+*/
+
+//import the relevant components
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './navbar';
 import HomePage from './homePage';
 import ReportsPage from "./reportsPage";
 
+//app component
 function App() {
     return (
-        <Router>
+        <Router> {/*set up a Router to manage routing */}
             <div className="App">
                 <Navbar/>
                 <div className="content">
-                    <Routes>
+                    <Routes> {/*set up the routing for different pages */}
                         <Route exact path="/" element={<HomePage/>}>
                         </Route>
                         <Route path="/report" element={<ReportsPage/>}>
